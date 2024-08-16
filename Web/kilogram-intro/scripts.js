edac4ee08f84e182c508cb127556a754c6354b92
Mikhael Jonathan <63639422+mikhaeljonathan@users.noreply.github.com>
2024-08-16 13:31:27 +0700

    change kilogram-intro script

--- Web/kilogram-intro/scripts.js
+++ Web/kilogram-intro/scripts.js
@@ -1,6 +1,6 @@
 function togglePost(id) {
     const src = document.getElementById(id).src;
-    if (src === "file:///Users/mj/kilogram/icons/like.svg") {
+    if (src.endsWith("icons/like.svg")) {
         document.getElementById(id).src = "icons/liked.svg";
     } else {
         document.getElementById(id).src = "icons/like.svg";
