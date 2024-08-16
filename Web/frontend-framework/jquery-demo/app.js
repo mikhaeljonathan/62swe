3944b7716bcbc3b4395cd622982c2ef61cc2e646
Mikhael Jonathan <63639422+mikhaeljonathan@users.noreply.github.com>
2024-08-16 13:30:04 +0700

    add jquery

---
+++ Web/frontend-framework/jquery-demo/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $("h1").text("Hello 62swe");
+    $(".paragraph").html("<strong>This is strong paragraph!</strong>");
+    $("#specific-paragraph").append("<br>This is appended text after specific paragraph");
+
+    $("#clickable-button").click(function(){
+        alert("Button clicked!");
+    });
+
+    $(".blue").hover(function(){
+        $(this).css("background-color", "red");
+    }, function() {
+        $(this).css("background-color", "blue");
+    })
+
+    $("#toggle-button").click(function() {
+        // $('.grey').hide();
+        // $('.grey').show();   
+        $('.grey').toggle();
+    })
+})
