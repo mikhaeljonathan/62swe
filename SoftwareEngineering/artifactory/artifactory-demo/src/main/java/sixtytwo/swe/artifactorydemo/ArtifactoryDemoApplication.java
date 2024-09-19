27ee3ae60c5690bb49b172178671c8188a9fd2de
Mikhael Jonathan <63639422+mikhaeljonathan@users.noreply.github.com>
2024-09-19 09:24:00 +0700

    add artifactory demo

---
+++ SoftwareEngineering/artifactory/artifactory-demo/src/main/java/sixtytwo/swe/artifactorydemo/ArtifactoryDemoApplication.java
@@ -0,0 +1,14 @@
+package sixtytwo.swe.artifactorydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtifactoryDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtifactoryDemoApplication.class, args);
+		System.out.println("Hello 62SWE");
+	}
+
+}
---
+++ SoftwareEngineering/artifactory/artifactory-demo/src/test/java/sixtytwo/swe/artifactorydemo/ArtifactoryDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sixtytwo.swe.artifactorydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtifactoryDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
